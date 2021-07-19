e6f380e854a1d195dd4b748e5012cd9324501210
desiandrade <desi.andrade@hotmail.com>
2021-07-19 14:29:55 -0300

    Removendo classes desnecessarias

--- src/main/java/br/com/letscode/java/moviebattleapi/imdbclient/MovieMinimal.java
+++ src/main/java/br/com/letscode/java/moviebattleapi/imdbclient/MovieMinimal.java
@@ -1,30 +1,30 @@
-package br.com.letscode.java.moviebattleapi.imdbclient;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.Arrays;
-
-public class MovieMinimal {
-
-    @JsonProperty("imdbID")
-    private String imdbId;
-    @JsonProperty("Title")
-    private String title;
-    private Integer year;
-
-    @JsonProperty("Year")
-    public void setYear(String year) {
-        this.year = convertYear(year);
-    }
-
-    private int convertYear(final String year) {
-        if (year.matches("\\d+")) {
-            return Integer.parseInt(year);
-        }
-        return Arrays.stream(year.split("\\D"))
-                .map(Integer::parseInt)
-                .findFirst()
-                .orElseThrow();
-    }
-
-}
+//package br.com.letscode.java.moviebattleapi.imdbclient;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//
+//import java.util.Arrays;
+//
+//public class MovieMinimal {
+//
+//    @JsonProperty("imdbID")
+//    private String imdbId;
+//    @JsonProperty("Title")
+//    private String title;
+//    private Integer year;
+//
+//    @JsonProperty("Year")
+//    public void setYear(String year) {
+//        this.year = convertYear(year);
+//    }
+//
+//    private int convertYear(final String year) {
+//        if (year.matches("\\d+")) {
+//            return Integer.parseInt(year);
+//        }
+//        return Arrays.stream(year.split("\\D"))
+//                .map(Integer::parseInt)
+//                .findFirst()
+//                .orElseThrow();
+//    }
+//
+//}
--- src/main/java/br/com/letscode/java/moviebattleapi/imdbclient/ResultSearch.java
+++ src/main/java/br/com/letscode/java/moviebattleapi/imdbclient/ResultSearch.java
@@ -1,23 +1,23 @@
-package br.com.letscode.java.moviebattleapi.imdbclient;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
-public class ResultSearch {
-
-    @JsonProperty("Search")
-    private List<MovieMinimal> resultList;
-    private Integer total;
-    private Boolean response;
-
-    @JsonProperty("Response")
-    public void setResponse(String response) {
-        this.response = Boolean.valueOf(response);
-    }
-
-    @JsonProperty("totalResults")
-    public void setTotal(String total) {
-        this.total = Integer.parseInt(total);
-    }
-}
+//package br.com.letscode.java.moviebattleapi.imdbclient;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//
+//import java.util.List;
+//
+//public class ResultSearch {
+//
+//    @JsonProperty("Search")
+//    private List<MovieMinimal> resultList;
+//    private Integer total;
+//    private Boolean response;
+//
+//    @JsonProperty("Response")
+//    public void setResponse(String response) {
+//        this.response = Boolean.valueOf(response);
+//    }
+//
+//    @JsonProperty("totalResults")
+//    public void setTotal(String total) {
+//        this.total = Integer.parseInt(total);
+//    }
+//}
