f895871e7be6778bc66c9e2227cc58ce448854d7
desiandrade <desi.andrade@hotmail.com>
2021-07-19 17:04:40 -0300

    Adicionando classe QuizClientAnswer

---
+++ src/main/java/br/com/letscode/java/moviebattleapi/quiz/QuizClientAnswer.java
@@ -0,0 +1,18 @@
+package br.com.letscode.java.moviebattleapi.quiz;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuizClientAnswer {
+
+    private String userIdQuiz;
+    private String password;
+    private String answer;
+}
